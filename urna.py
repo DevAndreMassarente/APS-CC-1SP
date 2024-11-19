023e32eab3928eb88f729f11a16590e2b2c164e4
Massarente <andre.massarente@unilever.com>
2024-11-19 20:41:35 -0300

    Restaurar arquivos para o estado inicial e corrigir erro 404

--- urna.py
+++ urna.py
@@ -24,29 +24,3 @@ if __name__ == '__main__':
         else:
             user_ip = "127.0.0.1" 
             print(votar(voto, user_ip))
-from criptografia_votos import criptografia, chave
-
-usuario_votou = {}
-
-def votar(voto, user_ip):
-    global usuario_votou
-    if user_ip in usuario_votou:
-        return "Você já votou!"
-    
-    votos_criptografados = criptografia(voto, chave)
-    
-    with open('criptografia_votos.txt', 'a', encoding='utf-8') as armazenamento_votos:
-        armazenamento_votos.write(votos_criptografados + '\n')
-    
-    usuario_votou[user_ip] = True
-    return f"Voto registrado: {voto}"
-
-if __name__ == '__main__':
-    while True:
-        voto = input("Insira o nome do seu time ou digite 'sair' para encerrar: ").strip().lower()
-        if voto == "sair":
-            print("Encerrando o programa.")
-            break
-        else:
-            user_ip = "127.0.0.1" 
-            print(votar(voto, user_ip))
