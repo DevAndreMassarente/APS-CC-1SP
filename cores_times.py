3ca1bb874d40b591035325ef7d2d4523ec6ce569
Massarente <andre.massarente@unilever.com>
2024-11-20 05:22:16 -0300

    Atualização para corrigir front end

--- cores_times.py
+++ cores_times.py
@@ -1,115 +1,115 @@
 cores_times = {
     # 50 clubes mais famosos do Brasil
-    "Corinthians": ["#000000", "#FFFFFF"],  # Preto, Branco
-    "Flamengo": ["#C3271D", "#000000"],  # Vermelho, Preto
-    "Palmeiras": ["#006437", "#FFFFFF"],  # Verde, Branco
-    "São Paulo": ["#FE0000", "#000000", "#FFFFFF"],  # Vermelho, Preto, Branco
-    "Santos": ["#000000", "#FFFFFF"],  # Preto, Branco
-    "Grêmio": ["#0D80BF", "#1F1A17", "#FFFFFF"],  # Azul, Preto, Branco
-    "Internacional": ["#E5050F", "#FFFFFF"],  # Vermelho, Branco
-    "Cruzeiro": ["#2F529E", "#FFFFFF"],  # Azul, Branco
-    "Atlético Mineiro": ["#000000", "#FFFFFF"],  # Preto, Branco
-    "Botafogo": ["#000000", "#FFFFFF"],  # Preto, Branco
-    "Vasco da Gama": ["#000000", "#FFFFFF"],  # Preto, Branco
-    "Fluminense": ["#008000", "#8B0000", "#FFFFFF"],  # Verde, Grená, Branco
-    "Bahia": ["#0033A0", "#FF0000", "#FFFFFF"],  # Azul, Vermelho, Branco
-    "Sport Recife": ["#FF0000", "#000000"],  # Vermelho, Preto
-    "Ceará": ["#000000", "#FFFFFF"],  # Preto, Branco
-    "Fortaleza": ["#0033A0", "#FF0000", "#FFFFFF"],  # Azul, Vermelho, Branco
-    "Atlético Paranaense": ["#FF0000", "#000000"],  # Vermelho, Preto
-    "Coritiba": ["#008000", "#FFFFFF"],  # Verde, Branco
-    "Goiás": ["#008000", "#FFFFFF"],  # Verde, Branco
-    "Vitória": ["#FF0000", "#000000"],  # Vermelho, Preto
-    "Chapecoense": ["#008000", "#FFFFFF"],  # Verde, Branco
-    "Avaí": ["#0033A0", "#FFFFFF"],  # Azul, Branco
-    "Figueirense": ["#000000", "#FFFFFF"],  # Preto, Branco
-    "Juventude": ["#008000", "#FFFFFF"],  # Verde, Branco
-    "Náutico": ["#FF0000", "#FFFFFF"],  # Vermelho, Branco
-    "Santa Cruz": ["#000000", "#FFFFFF", "#FF0000"],  # Preto, Branco, Vermelho
-    "Paysandu": ["#0033A0", "#FFFFFF"],  # Azul, Branco
-    "Remo": ["#0033A0", "#FFFFFF"],  # Azul, Branco
-    "ABC": ["#000000", "#FFFFFF"],  # Preto, Branco
-    "América Mineiro": ["#008000", "#000000"],  # Verde, Preto
-    "Botafogo-SP": ["#000000", "#FFFFFF", "#FF0000"],  # Preto, Branco, Vermelho
-    "Bragantino": ["#000000", "#FFFFFF"],  # Preto, Branco
-    "Criciúma": ["#FFD700", "#000000"],  # Amarelo, Preto
-    "Guarani": ["#008000", "#FFFFFF"],  # Verde, Branco
-    "Londrina": ["#0033A0", "#FFFFFF"],  # Azul, Branco
-    "Paraná Clube": ["#0033A0", "#FF0000", "#FFFFFF"],  # Azul, Vermelho, Branco
-    "Ponte Preta": ["#000000", "#FFFFFF"],  # Preto, Branco
-    "Sampaio Corrêa": ["#008000", "#FFD700", "#FF0000"],  # Verde, Amarelo, Vermelho
-    "São Caetano": ["#0033A0", "#FFFFFF"],  # Azul, Branco
-    "Vila Nova": ["#FF0000", "#FFFFFF"],  # Vermelho, Branco
-    "Atlético Goianiense": ["#FF0000", "#000000"],  # Vermelho, Preto
-    "CSA": ["#0033A0", "#FFFFFF"],  # Azul, Branco
-    "CRB": ["#FF0000", "#FFFFFF"],  # Vermelho, Branco
+    "corinthians": ["#000000", "#FFFFFF"],  # Preto, Branco
+    "flamengo": ["#C3271D", "#000000"],  # Vermelho, Preto
+    "palmeiras": ["#006437", "#FFFFFF"],  # Verde, Branco
+    "saopaulo": ["#FE0000", "#000000", "#FFFFFF"],  # Vermelho, Preto, Branco
+    "santos": ["#000000", "#FFFFFF"],  # Preto, Branco
+    "gremio": ["#0D80BF", "#1F1A17", "#FFFFFF"],  # Azul, Preto, Branco
+    "internacional": ["#E5050F", "#FFFFFF"],  # Vermelho, Branco
+    "cruzeiro": ["#2F529E", "#FFFFFF"],  # Azul, Branco
+    "atleticomineiro": ["#000000", "#FFFFFF"],  # Preto, Branco
+    "botafogo": ["#000000", "#FFFFFF"],  # Preto, Branco
+    "vascodagama": ["#000000", "#FFFFFF"],  # Preto, Branco
+    "fluminense": ["#008000", "#8B0000", "#FFFFFF"],  # Verde, Grená, Branco
+    "bahia": ["#0033A0", "#FF0000", "#FFFFFF"],  # Azul, Vermelho, Branco
+    "sportrecife": ["#FF0000", "#000000"],  # Vermelho, Preto
+    "ceara": ["#000000", "#FFFFFF"],  # Preto, Branco
+    "fortaleza": ["#0033A0", "#FF0000", "#FFFFFF"],  # Azul, Vermelho, Branco
+    "atleticoparanaense": ["#FF0000", "#000000"],  # Vermelho, Preto
+    "coritiba": ["#008000", "#FFFFFF"],  # Verde, Branco
+    "goias": ["#008000", "#FFFFFF"],  # Verde, Branco
+    "vitoria": ["#FF0000", "#000000"],  # Vermelho, Preto
+    "chapecoense": ["#008000", "#FFFFFF"],  # Verde, Branco
+    "avai": ["#0033A0", "#FFFFFF"],  # Azul, Branco
+    "figueirense": ["#000000", "#FFFFFF"],  # Preto, Branco
+    "juventude": ["#008000", "#FFFFFF"],  # Verde, Branco
+    "nautico": ["#FF0000", "#FFFFFF"],  # Vermelho, Branco
+    "santacruz": ["#000000", "#FFFFFF", "#FF0000"],  # Preto, Branco, Vermelho
+    "paysandu": ["#0033A0", "#FFFFFF"],  # Azul, Branco
+    "remo": ["#0033A0", "#FFFFFF"],  # Azul, Branco
+    "abc": ["#000000", "#FFFFFF"],  # Preto, Branco
+    "americamg": ["#008000", "#000000"],  # Verde, Preto
+    "botafogosp": ["#000000", "#FFFFFF", "#FF0000"],  # Preto, Branco, Vermelho
+    "bragantino": ["#000000", "#FFFFFF"],  # Preto, Branco
+    "criciuma": ["#FFD700", "#000000"],  # Amarelo, Preto
+    "guarani": ["#008000", "#FFFFFF"],  # Verde, Branco
+    "londrina": ["#0033A0", "#FFFFFF"],  # Azul, Branco
+    "paranaclube": ["#0033A0", "#FF0000", "#FFFFFF"],  # Azul, Vermelho, Branco
+    "pontepreta": ["#000000", "#FFFFFF"],  # Preto, Branco
+    "sampaiocorrea": ["#008000", "#FFD700", "#FF0000"],  # Verde, Amarelo, Vermelho
+    "saocaetano": ["#0033A0", "#FFFFFF"],  # Azul, Branco
+    "vilanova": ["#FF0000", "#FFFFFF"],  # Vermelho, Branco
+    "atleticogoianiense": ["#FF0000", "#000000"],  # Vermelho, Preto
+    "csa": ["#0033A0", "#FFFFFF"],  # Azul, Branco
+    "crb": ["#FF0000", "#FFFFFF"],  # Vermelho, Branco
 
     # 50 clubes mais famosos do mundo
-    "Barcelona": ["#004D98", "#A50044"],  # Azul, Grená
-    "Real Madrid": ["#FFFFFF"],  # Branco
-    "Manchester United": ["#DA291C", "#000000"],  # Vermelho, Preto
-    "Liverpool": ["#C8102E"],  # Vermelho
-    "Bayern de Munique": ["#DC052D", "#FFFFFF"],  # Vermelho, Branco
-    "Paris Saint-Germain": ["#004170", "#DA291C"],  # Azul, Vermelho
-    "Juventus": ["#000000", "#FFFFFF"],  # Preto, Branco
-    "Chelsea": ["#034694", "#FFFFFF"],  # Azul, Branco
-    "Manchester City": ["#6CABDD", "#FFFFFF"],  # Azul Claro, Branco
-    "Arsenal": ["#EF0107", "#FFFFFF"],  # Vermelho, Branco
-    "Milan": ["#FB090B", "#000000"],  # Vermelho, Preto
-    "Inter de Milão": ["#003366", "#000000"],  # Azul, Preto
-    "Ajax": ["#D42029", "#FFFFFF"],  # Vermelho, Branco
-    "Benfica": ["#FF0000", "#FFFFFF"],  # Vermelho, Branco
-    "Porto": ["#0033A0", "#FFFFFF"],  # Azul, Branco
-    "Boca Juniors": ["#0033A0", "#FFD700"],  # Azul, Amarelo
-    "River Plate": ["#FFFFFF", "#FF0000"],  # Branco, Vermelho
-    "Peñarol": ["#FFD700", "#000000"],  # Amarelo, Preto
-    "Nacional": ["#0033A0", "#FF0000", "#FFFFFF"],  # Azul, Vermelho, Branco
-    "Independiente": ["#FF0000", "#FFFFFF"],  # Vermelho, Branco
-    "Al Ahly": ["#FF0000", "#FFFFFF"],  # Vermelho, Branco
-    "Tottenham Hotspur": ["#FFFFFF", "#132257"],  # Branco, Azul
-    "Borussia Dortmund": ["#FDE100", "#000000"],  # Amarelo, Preto
-    "Atletico Madrid": ["#D7141A", "#FFFFFF"],  # Vermelho, Branco
-    "Sevilla": ["#FFFFFF", "#D7141A"],  # Branco, Vermelho
-    "Roma": ["#8E1B1B", "#FFD700"],  # Vermelho, Amarelo
-    "Lazio": ["#B2DFF4", "#FFFFFF"],  # Azul Claro, Branco
-    "Napoli": ["#0064A8", "#FFFFFF"],  # Azul Claro, Branco
-    "Galatasaray": ["#A32638", "#FFD700"],  # Vermelho, Amarelo
-    "Olympique de Marseille": ["#0080FF", "#FFFFFF"],  # Azul Claro, Branco
-    "Lyon": ["#004494", "#FFFFFF", "#D7141A"],  # Azul, Branco, Vermelho
-    "PSV Eindhoven": ["#DA291C", "#FFFFFF"],  # Vermelho, Branco
-    "Red Bull Salzburg": ["#FF0000", "#FFFFFF"],  # Vermelho, Branco
-    "Shakhtar Donetsk": ["#F36F21", "#000000"],  # Laranja, Preto
-    "Al Hilal": ["#005BAC", "#FFFFFF"],  # Azul, Branco
+    "barcelona": ["#004D98", "#A50044"],  # Azul, Grená
+    "realmadrid": ["#FFFFFF"],  # Branco
+    "manchesterunited": ["#DA291C", "#000000"],  # Vermelho, Preto
+    "liverpool": ["#C8102E", "#FFFFFF"],  # Vermelho
+    "bayerndemunique": ["#DC052D", "#FFFFFF"],  # Vermelho, Branco
+    "parissaintgermain": ["#004170", "#DA291C"],  # Azul, Vermelho
+    "juventus": ["#000000", "#FFFFFF"],  # Preto, Branco
+    "chelsea": ["#034694", "#FFFFFF"],  # Azul, Branco
+    "manchestercity": ["#6CABDD", "#FFFFFF"],  # Azul Claro, Branco
+    "arsenal": ["#EF0107", "#FFFFFF"],  # Vermelho, Branco
+    "milan": ["#FB090B", "#000000"],  # Vermelho, Preto
+    "interdemilao": ["#003366", "#000000"],  # Azul, Preto
+    "ajax": ["#D42029", "#FFFFFF"],  # Vermelho, Branco
+    "benfica": ["#FF0000", "#FFFFFF"],  # Vermelho, Branco
+    "porto": ["#0033A0", "#FFFFFF"],  # Azul, Branco
+    "bocajuniors": ["#0033A0", "#FFD700"],  # Azul, Amarelo
+    "riverplate": ["#FFFFFF", "#FF0000"],  # Branco, Vermelho
+    "penarol": ["#FFD700", "#000000"],  # Amarelo, Preto
+    "nacional": ["#0033A0", "#FF0000", "#FFFFFF"],  # Azul, Vermelho, Branco
+    "independiente": ["#FF0000", "#FFFFFF"],  # Vermelho, Branco
+    "alahly": ["#FF0000", "#FFFFFF"],  # Vermelho, Branco
+    "tottenhamhotspur": ["#FFFFFF", "#132257"],  # Branco, Azul
+    "borussiadortmund": ["#FDE100", "#000000"],  # Amarelo, Preto
+    "atleticomadrid": ["#D7141A", "#FFFFFF"],  # Vermelho, Branco
+    "sevilla": ["#FFFFFF", "#D7141A"],  # Branco, Vermelho
+    "roma": ["#8E1B1B", "#FFD700"],  # Vermelho, Amarelo
+    "lazio": ["#B2DFF4", "#FFFFFF"],  # Azul Claro, Branco
+    "napoli": ["#0064A8", "#FFFFFF"],  # Azul Claro, Branco
+    "galatasaray": ["#A32638", "#FFD700"],  # Vermelho, Amarelo
+    "olympiquedemarseille": ["#0080FF", "#FFFFFF"],  # Azul Claro, Branco
+    "lyon": ["#004494", "#FFFFFF", "#D7141A"],  # Azul, Branco, Vermelho
+    "psveindhoven": ["#DA291C", "#FFFFFF"],  # Vermelho, Branco
+    "redbullsalzburg": ["#FF0000", "#FFFFFF"],  # Vermelho, Branco
+    "shakhtardonetsk": ["#F36F21", "#000000"],  # Laranja, Preto
+    "alhilal": ["#005BAC", "#FFFFFF"],  # Azul, Branco
 
     # Times da NBA
-    "Atlanta Hawks": ["#E03A3E", "#C1D32F"],  # Vermelho, Verde
-    "Boston Celtics": ["#007A33", "#BA9653"],  # Verde, Dourado
-    "Brooklyn Nets": ["#000000", "#FFFFFF"],  # Preto, Branco
-    "Charlotte Hornets": ["#1D1160", "#00788C"],  # Roxo, Azul Claro
-    "Chicago Bulls": ["#CE1141", "#000000"],  # Vermelho, Preto
-    "Cleveland Cavaliers": ["#860038", "#FDBB30"],  # Vermelho, Dourado
-    "Dallas Mavericks": ["#00538C", "#002B5E"],  # Azul, Azul Escuro
-    "Denver Nuggets": ["#0E2240", "#FEC524"],  # Azul, Amarelo
-    "Detroit Pistons": ["#C8102E", "#1D42BA"],  # Vermelho, Azul
-    "Golden State Warriors": ["#1D428A", "#FFC72C"],  # Azul, Amarelo
-    "Houston Rockets": ["#CE1141", "#C4CED4"],  # Vermelho, Cinza
-    "Indiana Pacers": ["#002D62", "#FDBB30"],  # Azul, Amarelo
-    "LA Clippers": ["#C8102E", "#1D428A"],  # Vermelho, Azul
-    "Los Angeles Lakers": ["#552583", "#FDB927"],  # Roxo, Amarelo
-    "Memphis Grizzlies": ["#5D76A9", "#12173F"],  # Azul, Azul Escuro
-    "Miami Heat": ["#98002E", "#F9A01B"],  # Vermelho, Amarelo
-    "Milwaukee Bucks": ["#00471B", "#EEE1C6"],  # Verde, Creme
-    "Minnesota Timberwolves": ["#0C2340", "#236192"],  # Azul, Azul Claro
-    "New Orleans Pelicans": ["#0C2340", "#C8102E"],  # Azul, Vermelho
-    "New York Knicks": ["#006BB6", "#F58426"],  # Azul, Laranja
-    "Oklahoma City Thunder": ["#007AC1", "#EF3B24"],  # Azul, Laranja
-    "Orlando Magic": ["#0077C0", "#C4CED4"],  # Azul, Cinza
-    "Philadelphia 76ers": ["#006BB6", "#ED174C"],  # Azul, Vermelho
-    "Phoenix Suns": ["#1D1160", "#E56020"],  # Roxo, Laranja
-    "Portland Trail Blazers": ["#E03A3E", "#000000"],  # Vermelho, Preto
-    "Sacramento Kings": ["#5A2D81", "#63727A"],  # Roxo, Cinza
-    "San Antonio Spurs": ["#C4CED4", "#000000"],  # Cinza, Preto
-    "Toronto Raptors": ["#CE1141", "#000000"],  # Vermelho, Preto
-    "Utah Jazz": ["#002B5C", "#F9A01B"],  # Azul, Amarelo
-    "Washington Wizards": ["#002B5C", "#E31837"],  # Azul, Vermelho
+    "atlantahawks": ["#E03A3E", "#C1D32F"],  # Vermelho, Verde
+    "bostonceltics": ["#007A33", "#BA9653"],  # Verde, Dourado
+    "brooklynnets": ["#000000", "#FFFFFF"],  # Preto, Branco
+    "charlottehornets": ["#1D1160", "#00788C"],  # Roxo, Azul Claro
+    "chicagobulls": ["#CE1141", "#000000"],  # Vermelho, Preto
+    "clevelandcavaliers": ["#860038", "#FDBB30"],  # Vermelho, Dourado
+    "dallasmavericks": ["#00538C", "#002B5E"],  # Azul, Azul Escuro
+    "denvernuggets": ["#0E2240", "#FEC524"],  # Azul, Amarelo
+    "detroitpistons": ["#C8102E", "#1D42BA"],  # Vermelho, Azul
+    "goldenstatewarriors": ["#1D428A", "#FFC72C"],  # Azul, Amarelo
+    "houstonrockets": ["#CE1141", "#C4CED4"],  # Vermelho, Cinza
+    "indianapacers": ["#002D62", "#FDBB30"],  # Azul, Amarelo
+    "laclippers": ["#C8102E", "#1D428A"],  # Vermelho, Azul
+    "losangeleslakers": ["#552583", "#FDB927"],  # Roxo, Amarelo
+    "memphisgrizzlies": ["#5D76A9", "#12173F"],  # Azul, Azul Escuro
+    "miamiheat": ["#98002E", "#F9A01B"],  # Vermelho, Amarelo
+    "milwaukeebucks": ["#00471B", "#EEE1C6"],  # Verde, Creme
+    "minnesotatimberwolves": ["#0C2340", "#236192"],  # Azul, Azul Claro
+    "neworleanspelicans": ["#0C2340", "#C8102E"],  # Azul, Vermelho
+    "newyorkknicks": ["#006BB6", "#F58426"],  # Azul, Laranja
+    "oklahomacitythunder": ["#007AC1", "#EF3B24"],  # Azul, Laranja
+    "orlandomagic": ["#0077C0", "#C4CED4"],  # Azul, Cinza
+    "philadelphia76ers": ["#006BB6", "#ED174C"],  # Azul, Vermelho
+    "phoenixsuns": ["#1D1160", "#E56020"],  # Roxo, Laranja
+    "portlandtrailblazers": ["#E03A3E", "#000000"],  # Vermelho, Preto
+    "sacramentokings": ["#5A2D81", "#63727A"],  # Roxo, Cinza
+    "sanantoniospurs": ["#C4CED4", "#000000"],  # Cinza, Preto
+    "torontoraptors": ["#CE1141", "#000000"],  # Vermelho, Preto
+    "utahjazz": ["#002B5C", "#F9A01B"],  # Azul, Amarelo
+    "washingtonwizards": ["#002B5C", "#E31837"],  # Azul, Vermelho
 }
